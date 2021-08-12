db16a6e8767ce56e6d82994efa2d36d8eb1f38dd
Manali Mathur <a.manalimathur@gmail.com>
2021-08-12 17:17:14 +0530

    add project schemas and api setup

---
+++ server/database/allModels.js
@@ -0,0 +1,17 @@
+import { FoodModel } from "./food";
+import { RestaurantModel } from "./restaurant";
+import { OrderModel } from "./order";
+import { MenuModel } from "./menu";
+import { ReviewModel } from "./reviews";
+import { UserModel } from "./user";
+import { ImageModel } from "./image";
+
+export {
+    FoodModel,
+    RestaurantModel,
+    OrderModel,
+    MenuModel,
+    ReviewModel,
+    UserModel,
+    ImageModel
+}
