1e8de8f658ee43f4003a8b95d7b620a3964d2091
Manali Mathur <a.manalimathur@gmail.com>
2021-08-13 15:00:59 +0530

    add statics and methods, refactor signup route

--- server/API/Auth/index.js
+++ server/API/Auth/index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 // Models
@@ -16,28 +15,14 @@ Method          POST
 */
 Router.post("/signup", async (req, res) => {
     try {
-        const { email, password, fullname, phoneNumber } = req.body.credentials;
-
-        // check whether user exist
-        const checkUserByEmail = await UserModel.findOne({ email })
-        const checkUserByPhone = await UserModel.findOne({ phoneNumber })
-
-        if (checkUserByEmail || checkUserByPhone) {
-            return res.json({ error: "User already exist!" })
-        }
-
-        // hash the password
-        const bcryptSalt = await bcrypt.genSalt(8);
-
-        const hashedPassword = await bcrypt.hash(password, bcryptSalt);
+        await UserModel.findByEmailAndPhone(req.body.credentials);
 
         // save to database
-        await UserModel.create({ ...req.body.credentials, password: hashedPassword });
+        const newUser = await UserModel.create(req.body.credentials);
 
         // generate JWT auth token
-        const token = jwt.sign({ user: { fullname, email } }, "ZomatoMaster");
+        const token = newUser.generateJwtToken();
 
-        // return
         return res.status(200).json({ token, status: "Success!" });
     } catch (error) {
         return res.status(500).json({ error: error.message });
--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
+require("dotenv").config()
+
 import express from "express";
 import cors from "cors";
 import helmet from "helmet"
+
+// microservice routes
+import Auth from "./API/Auth";
+
+// Database connection 
+import ConnectDB from "./database/connection";
 
 const zomato = express();
 const PORT = 4000;
@@ -11,8 +19,13 @@ zomato.use(express.urlencoded({ extended: false }))
 zomato.use(cors());
 zomato.use(helmet())
 
+// application routes
+zomato.use("/auth", Auth);
+
 zomato.get("/", (req, res) => res.json({ message: "Setup success!" }))
 
 zomato.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+    ConnectDB()
+        .then(() => console.log(`Server is running on port ${PORT}`))
+        .catch(() => console.log("Server is running, but database connection failed"))
 })
