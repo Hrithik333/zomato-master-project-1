87644700fcc25ec5f750e4e8959e08e9e34ad61c
Manali Mathur <a.manalimathur@gmail.com>
2021-09-04 07:03:00 +0530

    fix navbar

--- client/src/Components/Navbar/index.js
+++ client/src/Components/Navbar/index.js
@@ -26,32 +26,34 @@ const MobileNav = () => {
     )
 }
 
-const MediumNav = () => {
+const LargeNav = () => {
     return (
-        <div className="w-full p-4 lg:px-16 flex items-center gap-4 ">
-            <div className="w-32">
-                <img
-                    src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png"
-                    alt="logo"
-                    className="w-full h-full"
-                />
-            </div>
-            <div className="w-full flex items-center gap-3 bg-white shadow-md p-3 border border-gray-200 rounded lg:w-2/3">
-                <div className="flex items-center gap-2 border-r-2 border-gray-300 pr-2">
-                    <span className="text-zomato-400">
-                        <HiLocationMarker />
-                    </span>
-                    <input type="text" placeholder="Bengaluru" className="focus:outline-none" />
-                    <IoMdArrowDropdown />
+        <div className="container mx-auto px-20">
+            <div className="w-full flex items-center py-4 gap-4 ">
+                <div className="w-44">
+                    <img
+                        src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png"
+                        alt="logo"
+                        className="w-full h-full"
+                    />
                 </div>
-                <div className="flex items-center gap-2 w-full">
-                    <RiSearch2Line />
-                    <input type="search" placeholder="Search for restaurant, cuisine or a dish" className="focus:outline-none w-full" />
+                <div className="w-full flex items-center gap-3 bg-white shadow-md p-3 border border-gray-200 rounded w-2/3">
+                    <div className="flex items-center gap-2 border-r-2 border-gray-300 mr-2 pr-2">
+                        <span className="text-zomato-400">
+                            <HiLocationMarker />
+                        </span>
+                        <input type="text" placeholder="Bengaluru" className="focus:outline-none" />
+                        <IoMdArrowDropdown />
+                    </div>
+                    <div className="flex items-center gap-2 w-full">
+                        <RiSearch2Line />
+                        <input type="search" placeholder="Search for restaurant, cuisine or a dish" className="focus:outline-none w-full" />
+                    </div>
                 </div>
-            </div>
-            <div className="flex gap-4 lg:gap-8 lg:pl-20">
-                <button className="text-gray-500 text-xl hover:text-gray-800">Login</button>
-                <button className="text-gray-500 text-xl hover:text-gray-800">Signup</button>
+                <div className="flex gap-4 lg:gap-8 lg:pl-20">
+                    <button className="text-gray-500 text-xl hover:text-gray-800">Login</button>
+                    <button className="text-gray-500 text-xl hover:text-gray-800">Signup</button>
+                </div>
             </div>
         </div>
     )
@@ -61,11 +63,11 @@ const Navbar = () => {
     return (
         <>
             <nav className="bg-white">
-                <div className="md:hidden">
+                <div className="lg:hidden">
                     <MobileNav />
                 </div>
-                <div className="hidden md:block">
-                    <MediumNav />
+                <div className="hidden lg:block">
+                    <LargeNav />
                 </div>
             </nav>
         </>
