654989b03dd8905aae2e773e156b72877b334cb8
Manali Mathur <a.manalimathur@gmail.com>
2021-09-05 20:19:00 +0530

    add delivery section

--- client/src/Components/Delivery/index.js
+++ client/src/Components/Delivery/index.js
@@ -1,14 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // Components
 import DeliveryCarousel from './DeliveryCarousel';
 import RestaurantCard from '../RestaurantCard';
 
 const Delivery = () => {
+    const [restaurantList, setRestaurantList] = useState([
+        {
+            _id: "123456",
+            photos: [
+                "https://b.zmtcdn.com/data/pictures/7/19722897/11371bfbad32252cddfdcf2989936ef8_o2_featured_v2.jpg?output-format=webp"
+            ],
+            name: "La Pino'z Pizza",
+            cuisine: ["Pizza", "Fast Food", "Beverages"],
+            isPro: false,
+            isOff: false,
+            averageCost: 150,
+            durationOfDelivery: 41,
+            restaurantReviewValue: 4.1
+        },
+        {
+            _id: "123456-2",
+            photos: [
+                "https://b.zmtcdn.com/data/pictures/8/18694788/ba50a5176f9b3abf84a4b734543474a2.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*"
+            ],
+            name: "Pizza Hut",
+            cuisine: ["Pizza", "Fast Food", "Desserts"],
+            isPro: true,
+            isOff: 75,
+            averageCost: 150,
+            durationOfDelivery: 50,
+            restaurantReviewValue: 4.1
+        },
+        {
+            _id: "123456-3",
+            photos: [
+                "https://b.zmtcdn.com/data/pictures/0/18818190/8b785eecf26ae650bd21307fa92fbe70.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*"
+            ],
+            name: "Bake and Bite",
+            cuisine: ["Bakery", "Desserts", "Fast Food"],
+            isPro: false,
+            isOff: 75,
+            averageCost: 150,
+            durationOfDelivery: 41,
+            restaurantReviewValue: 4.1
+        },
+
+    ]);
+
     return (
         <>
             <DeliveryCarousel />
-            <RestaurantCard />
+            <div className="flex flex-wrap justify-between">
+                {restaurantList.map((restaurant) => (
+                    <RestaurantCard {...restaurant} />
+                ))}
+
+            </div>
         </>
     )
 };
--- client/src/Components/RestaurantCard.js
+++ client/src/Components/RestaurantCard.js
@@ -1,30 +1,40 @@
 import React from 'react';
 import { AiTwotoneStar } from "react-icons/ai";
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
     return (
         <>
-            <div className="bg-white p-2 rounded-2xl transition duration-700 ease-in-out hover:shadow-lg w-full md:w-1/2 lg:w-1/3 mb-40">
+            <div className="bg-white p-4 mb-4 rounded-2xl transition duration-700 ease-in-out hover:shadow-lg w-full md:w-1/2 lg:w-1/3">
                 <div className="w-full h-56 lg:h-64 relative">
                     <div className="absolute bottom-4 w-full flex items-end justify-between">
                         <div className="flex flex-col gap-2 items-start">
-                            <span className="bg-zomato-400 text-white px-2 py-1 text-sm rounded">₹75 OFF</span>
-                            <span className="bg-blue-600 text-white px-2 py-1 text-sm rounded">Pro extra 10% off</span>
+                            {props.isOff && (
+                                <span className="bg-zomato-400 text-white px-2 py-1 text-sm rounded">
+                                    ₹{`${props.isOff}`} OFF
+                                </span>
+                            )}
+                            {props.isPro && (
+                                <span className="bg-blue-600 text-white px-2 py-1 text-sm rounded">
+                                    Pro extra 10% off
+                                </span>
+                            )}
                         </div>
-                        <span className="bg-white bg-opacity-75 p-1 rounded mr-3">42 min</span>
+                        <span className="bg-white bg-opacity-75 p-1 rounded mr-3">
+                            {props.durationOfDelivery} min
+                        </span>
                     </div>
-                    <img src="https://b.zmtcdn.com/data/pictures/7/19722897/11371bfbad32252cddfdcf2989936ef8_o2_featured_v2.jpg?output-format=webp" alt="food" className="rounded-2xl w-full h-full" />
+                    <img src={props.photos.length && props.photos[0]} alt="restaurant" className="rounded-2xl w-full h-full" />
                 </div>
                 <div className="flex flex-col gap-2 my-2">
                     <div className="flex items-center justify-between">
-                        <h4 className="text-xl font-medium">La Pino'z Pizza</h4>
+                        <h4 className="text-xl font-medium">{props.name}</h4>
                         <span className="bg-green-800 text-sm text-white p-1 rounded flex items-center">
-                            4.1 <AiTwotoneStar />
+                            {props.restaurantReviewValue} <AiTwotoneStar />
                         </span>
                     </div>
                     <div className="flex items-center justify-between text-gray-500">
-                        <h4>Pizza, Fast Food, Beverages</h4>
-                        <p>₹150 for one</p>
+                        <h4>{props.cuisine.join(", ")}</h4>
+                        <p>₹{props.averageCost} for one</p>
                     </div>
                 </div>
             </div>
