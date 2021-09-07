6ff95c80722b7bc798901b23d943c5350e6a5533
Manali Mathur <a.manalimathur@gmail.com>
2021-09-08 02:22:36 +0530

    add restaurant info

--- client/src/Layout/Restaurant.layout.js
+++ client/src/Layout/Restaurant.layout.js
@@ -3,6 +3,7 @@ import React from 'react';
 // components
 import RestaurantNavbar from '../Components/Navbar/RestaurantNavbar';
 import ImageGrid from '../Components/Restaurant/ImageGrid';
+import RestaurantInfo from '../Components/Restaurant/RestaurantInfo';
 
 const RestaurantLayout = () => {
     return (
@@ -16,6 +17,13 @@ const RestaurantLayout = () => {
                     "https://b.zmtcdn.com/data/reviews_photos/afd/037265da4eb2adae82c71a72a21eeafd_1629791462.jpg?output-format=webp&fit=around|300:273&crop=300:273;*,*",
                     "https://b.zmtcdn.com/data/reviews_photos/985/2265ea26bf223db5e976075635ab8985_1622643290.jpg?output-format=webp&fit=around|771.75:416.25&crop=771.75:416.25;*,*"
                 ]} />
+                <RestaurantInfo
+                    name="La Pino'z Pizza"
+                    restaurantRating="3.8"
+                    deliveryRating="4.1"
+                    cuisine="Pizza, Fast Food, Beverages"
+                    address="Sardarpura, Jodhpur"
+                />
             </div>
         </>
     )
