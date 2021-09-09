c2343e7966f1a2e029f11ac4e6d54c9fe6a24322
Manali Mathur <a.manalimathur@gmail.com>
2021-09-09 09:23:43 +0530

    add food item card

--- client/src/Components/Restaurant/OrderOnline/MenuCategory.js
+++ client/src/Components/Restaurant/OrderOnline/MenuCategory.js
@@ -4,7 +4,7 @@ import React from 'react';
 const MenuCategory = (props) => {
     return (
         <>
-            <div className={classNames({
+            <div className={classNames("cursor-pointer", {
                 "text-zomato-400 bg-zomato-50 border-r-4 border-zomato-400 py-1 px-2": props.isActive
             })}
 
--- client/src/Page/Restaurant/OrderOnline.js
+++ client/src/Page/Restaurant/OrderOnline.js
@@ -1,19 +1,36 @@
 import React from 'react';
+import { AiOutlineCompass } from 'react-icons/ai';
+import { BiTime } from 'react-icons/bi';
 
 // components
 import FloatingMenuButton from '../../Components/Restaurant/OrderOnline/FloatingMenuButton';
+import FoodItemCard from '../../Components/Restaurant/OrderOnline/FoodItemCard';
 import MenuListContainer from '../../Components/Restaurant/OrderOnline/MenuListContainer';
 
 const OrderOnline = () => {
     return (
         <>
-            <div className="w-full">
-                <aside className="hidden md:flex flex-col gap-3 border-r border-gray-400 w-1/4">
+            <div className="w-full flex">
+                <aside className="hidden md:flex flex-col gap-3 border-r border-gray-200 overflow-y-scroll h-screen w-1/4">
                     <MenuListContainer />
                     <MenuListContainer />
                     <MenuListContainer />
                 </aside>
                 <div className="w-full md:w-3/4">
+                    <h2 className="text-xl md:text-2xl font-semibold">Order Online</h2>
+                    <h4 className="flex items-center gap-2 font-light text-gray-500">
+                        <AiOutlineCompass /> Live track your order | <BiTime /> 45 min
+                    </h4>
+
+                    <section>
+                        <FoodItemCard
+                            image="https://b.zmtcdn.com/data/dish_photos/5cb/97b3ae9bc4d000d22a42a6a41ea795cb.jpg?output-format=webp&fit=around|130:130&crop=130:130;*,*"
+                            title="Margherita Pizza"
+                            price="110"
+                            rating={3}
+                            description="Say cheese"
+                        />
+                    </section>
 
                 </div>
             </div>
